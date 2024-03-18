16394cad9650cdfcbe68dff2dfcaa570e99cb9f5
evagg <evagggianna@gmail.com>
2024-03-18 16:07:56 +0200

    CSS added fo correct and incorrect typing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+
+  onInput(value: string) {
+      this.enteredText = value;
+  }
+
 }
